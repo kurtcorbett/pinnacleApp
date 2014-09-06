bc8d77724c13d947936b126fa152392c9ddf7e08
kurtcorbett <kurtcorbett@gmail.com>
2014-09-06 13:33:24 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,41 +1,63 @@
-var knex = require('knex')({
-    client: 'pg',
-    connection: {
-        host     : '127.0.0.1',
-        user     : 'postgres',
-        password : 'postgres',
-        database : 'testdb',
-        charset  : 'utf8'
-    },
-    debug: true
-});
-
 var express = require("express");
-//var Bookshelf = require('bookshelf')(knex);
+
+var Sequelize = require('sequelize')
+    , sequelize = new Sequelize('pinnacle', 'postgres', 'postgres', {
+        dialect: "postgres", // or 'sqlite', 'postgres', 'mariadb'
+        port:    5432
+    });
 
 var app = express();
+console.log('getting something');
 
 
 
-//var user = Bookshelf.Model({
-//    tableName : "users"
-//});
-//
-//var users = Bookshelf.Collection({
-//    model : user
-//});
 
-app.get('/', function(req,res) {
-
-    knex.select('*').from('users').then(function(users) {
-        console.log(users);
-        res.json(users);
+sequelize
+    .authenticate()
+    .complete(function(err) {
+        if (!!err) {
+            console.log('Unable to connect to the database:', err)
+        } else {
+            console.log('Connection has been established successfully.')
+        }
     });
+
+sequelize
+    .sync({ force: true })
+    .complete(function(err) {
+        if (!!err) {
+            console.log('An error occurred while creating the table:', err)
+        } else {
+            console.log('It worked!')
+        }
+    });
+
+var User = sequelize.define('User', {
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    position: Sequelize.STRING,
+    hireDate: Sequelize.DATE
+}, {
+    tableName: 'users', // this will define the table's name
+    timestamps: false           // this will deactivate the timestamp columns
 });
 
-app.post('')
+User
+    .create({
+        firstName: 'bob',
+        lastName: 'builder',
+        position: 'inside sales',
+        hireDate: 2013-01-01
+    })
+    .complete(function(err, user) {
+        console.log("user created!");
+        /* ... */
+    });
+
+
+
+
 
 app.listen(3000);
-
 
 
